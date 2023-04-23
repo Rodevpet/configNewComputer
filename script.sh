616493d4d15fe33ef4e0fab904cffd2f2805616e
XxropttxX <42070219+Rodevpet@users.noreply.github.com>
2023-04-23 19:40:01 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -35,6 +35,8 @@ if [ $(id -u ) = 0 ] ; then
 	brew cleanup
 	#moving neovim config file
 	cp -r ~/configNewComputer/usingFiles/nvim ~/.config/
+	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 else 
 	echo "You got to start the script as root"
 	exit 1 
